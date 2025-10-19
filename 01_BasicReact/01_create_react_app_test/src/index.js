3b85a3502b377156a11e372f8516a960a27f9455
Krishnendu-27 <kai27deep03@gmail.com>
2025-10-19 06:10:15 +0530

    fic the erooor

--- 01_BasicReact/01_create_react_app_test/src/index.js
+++ 01_BasicReact/01_create_react_app_test/src/index.js
@@ -1,9 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
-
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+const container = document.getElementById("root");
+// React 18+ uses createRoot instead of ReactDOM.render
+const root = createRoot(container);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
