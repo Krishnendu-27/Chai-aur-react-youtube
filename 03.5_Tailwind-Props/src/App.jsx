29bb102e2b5ade32cdda218dc945f58893b9f9eb
Krishnendu-27 <kai27deep03@gmail.com>
2025-10-23 22:20:02 +0530

    steup tailwind css for props project

---
+++ 03.5_Tailwind-Props/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      {/* this process inlcude for tailwind install in vite 
+    
+    
+    */}
+      <h1>hallo bro</h1>
+    </>
+  );
+}
+
+export default App;
